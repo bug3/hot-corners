4c09de3121eba9ba2dc8ac96bff0cb2f92760b4b
bug3 <bug3dev@gmail.com>
2022-07-11 01:50:48 +0300

    Define install func

--- setup.sh
+++ setup.sh
@@ -2,6 +2,20 @@
 
 source config.conf
 
+install () {
+    if [[ ! -e $appPath ]]; then
+        sudo mkdir $appPath
+    else
+        echo "$appPath already exists"
+        exit 1
+    fi
+
+    sudo cp -r * $appPath
+
+    sudo ln -s $appPath$binFile $binFile
+    sudo chmod +x $appPath$binFile
+}
+
 if [[ $# -eq 0 ]]; then
 	install
 elif [[ $1 == "-r" ]]; then
