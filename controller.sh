3203dba0f23b167bc55b2b51644dc41ec5825fb5
bug3 <bug3dev@gmail.com>
2022-07-11 00:06:25 +0300

    Check corners

--- controller.sh
+++ controller.sh
@@ -3,3 +3,23 @@
 sens=2
 displayWidth=$(xdotool getdisplaygeometry | awk '{ print $1 }')
 displayHeight=$(xdotool getdisplaygeometry | awk '{ print $2 }')
+
+while true; do
+    eval $(xdotool getmouselocation --shell)
+
+    if [[ $X -lt $((sens - 1)) && $Y -lt $((sens - 1)) ]]; then
+        notify-send "top-left"
+        sleep 1
+    elif [[ $X -lt $((sens - 1)) && $Y -gt $((displayHeight - sens)) ]]; then
+        notify-send "bottom-left"
+        sleep 1
+    elif [[ $X -gt $((displayWidth - sens)) && $Y -gt $((displayHeight - sens)) ]]; then
+        notify-send "bottom-right"
+        sleep 1
+    elif [[ $X -gt $((displayWidth - sens)) && $Y -lt $((sens - 1)) ]]; then
+        notify-send "top-right"
+        sleep 1
+    fi
+
+    sleep 0.3
+done
