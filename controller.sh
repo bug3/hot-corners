3d3627cb934dfe3c2fb4b2b2348f2c759c58b28c
bug3 <bug3dev@gmail.com>
2022-07-12 19:32:36 +0300

    Exec corner scripts

--- controller.sh
+++ controller.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source $(dirname $(realpath $0))/config.conf
+
 sens=2
 displayWidth=$(xdotool getdisplaygeometry | awk '{ print $1 }')
 displayHeight=$(xdotool getdisplaygeometry | awk '{ print $2 }')
@@ -8,16 +10,16 @@ while true; do
     eval $(xdotool getmouselocation --shell)
 
     if [[ $X -lt $((sens - 1)) && $Y -lt $((sens - 1)) ]]; then
-        notify-send "top-left"
+        /bin/bash $appPath/corners/top-left.sh
         sleep 1
     elif [[ $X -lt $((sens - 1)) && $Y -gt $((displayHeight - sens)) ]]; then
-        notify-send "bottom-left"
+        /bin/bash $appPath/corners/bottom-left.sh
         sleep 1
     elif [[ $X -gt $((displayWidth - sens)) && $Y -gt $((displayHeight - sens)) ]]; then
-        notify-send "bottom-right"
+        /bin/bash $appPath/corners/bottom-right.sh
         sleep 1
     elif [[ $X -gt $((displayWidth - sens)) && $Y -lt $((sens - 1)) ]]; then
-        notify-send "top-right"
+        /bin/bash $appPath/corners/top-right.sh
         sleep 1
     fi
 
