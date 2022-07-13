4c4530a5c6066b176413f988392074641f48c04c
bug3 <bug3dev@gmail.com>
2022-07-13 13:15:10 +0300

    Edit vars

--- controller.sh
+++ controller.sh
@@ -2,26 +2,26 @@
 
 source $(dirname $(realpath $0))/config.conf
 
-sens=2
+cSize=$((cornerSize + 1))
 displayWidth=$(xdotool getdisplaygeometry | awk '{ print $1 }')
 displayHeight=$(xdotool getdisplaygeometry | awk '{ print $2 }')
 
 while true; do
     eval $(xdotool getmouselocation --shell)
 
-    if [[ $X -lt $((sens - 1)) && $Y -lt $((sens - 1)) ]]; then
+    if [[ $X -lt $((cSize - 1)) && $Y -lt $((cSize - 1)) ]]; then
         /bin/bash $appPath/corners/top-left.sh
-        sleep 1
-    elif [[ $X -lt $((sens - 1)) && $Y -gt $((displayHeight - sens)) ]]; then
+        sleep $execDelay
+    elif [[ $X -lt $((cSize - 1)) && $Y -gt $((displayHeight - cSize)) ]]; then
         /bin/bash $appPath/corners/bottom-left.sh
-        sleep 1
-    elif [[ $X -gt $((displayWidth - sens)) && $Y -gt $((displayHeight - sens)) ]]; then
+        sleep $execDelay
+    elif [[ $X -gt $((displayWidth - cSize)) && $Y -gt $((displayHeight - cSize)) ]]; then
         /bin/bash $appPath/corners/bottom-right.sh
-        sleep 1
-    elif [[ $X -gt $((displayWidth - sens)) && $Y -lt $((sens - 1)) ]]; then
+        sleep $execDelay
+    elif [[ $X -gt $((displayWidth - cSize)) && $Y -lt $((cSize - 1)) ]]; then
         /bin/bash $appPath/corners/top-right.sh
-        sleep 1
+        sleep $execDelay
     fi
 
-    sleep 0.3
+    sleep $controlDelay
 done
