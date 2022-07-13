98fd0ea49f165cd9e7f3b398ac2536573e7916a5
bug3 <bug3dev@gmail.com>
2022-07-13 14:03:06 +0300

    Config execCornerScript func

--- controller.sh
+++ controller.sh
@@ -2,25 +2,31 @@
 
 source $(dirname $(realpath $0))/config.conf
 
+lastCorner="none"
 cSize=$((cornerSize + 1))
 displayWidth=$(xdotool getdisplaygeometry | awk '{ print $1 }')
 displayHeight=$(xdotool getdisplaygeometry | awk '{ print $2 }')
+
+execCornerScript() {
+    if [[ $lastCorner != $1 ]]; then
+        lastCorner=$1
+        /bin/bash $appPath/corners/$1.sh
+    fi
+}
 
 while true; do
     eval $(xdotool getmouselocation --shell)
 
     if [[ $X -lt $((cSize - 1)) && $Y -lt $((cSize - 1)) ]]; then
-        /bin/bash $appPath/corners/top-left.sh
-        sleep $execDelay
+        execCornerScript "top-left"
     elif [[ $X -lt $((cSize - 1)) && $Y -gt $((displayHeight - cSize)) ]]; then
-        /bin/bash $appPath/corners/bottom-left.sh
-        sleep $execDelay
+        execCornerScript "bottom-left"
     elif [[ $X -gt $((displayWidth - cSize)) && $Y -gt $((displayHeight - cSize)) ]]; then
-        /bin/bash $appPath/corners/bottom-right.sh
-        sleep $execDelay
+        execCornerScript "bottom-right"
     elif [[ $X -gt $((displayWidth - cSize)) && $Y -lt $((cSize - 1)) ]]; then
-        /bin/bash $appPath/corners/top-right.sh
-        sleep $execDelay
+        execCornerScript "top-right"
+    else
+        lastCorner="none"
     fi
 
     sleep $controlDelay
